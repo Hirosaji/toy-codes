7607581f107980de467014c83c746527e87f49ad
Hiroya Kato <a12.anr7@g.chuo-u.ac.jp>
2018-05-02 15:19:45 +0900

    update (temp)

--- macMap/macMap.js
+++ macMap/macMap.js
@@ -10,12 +10,27 @@ function handlingJson() {
 	  .await(function(error, json) {
 	  	if (error) throw error;
 
-	  	// var outputJSON = Object.assign({}, json);
-	  	// outputJSON.features = [];
+	  	var outputJSON = {
+	  		"type": "FeatureCollection",
+			"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+			"features": [],
+	  	};
+	  	outputJSON.features = [];
 
 	  	json.forEach(function(value, i){
 	  		console.log(value);
 	  	})
+
+        // JSON出力
+        var data = JSON.stringify(outputGeoJSON);
+        var a = document.createElement('a');
+        a.textContent = 'export';
+        a.download = 'outputGeoJSON.json';
+        a.href = window.URL.createObjectURL(new Blob([data], { type: 'text/plain' }));
+        a.dataset.downloadurl = ['text/plain', a.download, a.href].join(':');
+
+        var exportLink = document.getElementById('export-link');
+        exportLink.appendChild(a);
 
 	});
 };
