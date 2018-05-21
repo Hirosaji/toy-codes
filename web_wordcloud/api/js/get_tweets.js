60c215c79b8ae027a54f300b61021d982d4e9cc3
Hiroya Kato <a12.anr7@g.chuo-u.ac.jp>
2018-05-21 14:16:11 +0900

    adopt-promise

--- web_wordcloud/api/js/get_tweets.js
+++ web_wordcloud/api/js/get_tweets.js
@@ -18,27 +18,31 @@ app.get('/', function(req, res) {
 
 	function get_tweets(){
 
-		let client = new twitterClient({
-		    consumer_key:        credentials.twitter_API_Key,
-		    consumer_secret:     credentials.twitter_API_Secret,
-		    access_token_key:    credentials.twitter_Access_Token,
-		    access_token_secret: credentials.twitter_Access_Token_Secret,
-		});
+		return new Promise(function(resolve){
 
-		let params = {
-			user_id: credentials.twitter_userID,
-			count: 200,
-		};
+			let client = new twitterClient({
+			    consumer_key:        credentials.twitter_API_Key,
+			    consumer_secret:     credentials.twitter_API_Secret,
+			    access_token_key:    credentials.twitter_Access_Token,
+			    access_token_secret: credentials.twitter_Access_Token_Secret,
+			});
 
-		client.get('statuses/user_timeline', params, function(error, tweets, response){
+			let params = {
+				user_id: credentials.twitter_userID,
+				count: 200,
+			};
 
-			// // ローカルテスト用
-			// const format_tweets_json = JSON.stringify(tweets);
-			// const format_response_json = JSON.stringify(response)
-			// fs.writeFile('json/tweets.json', format_tweets_json)
-			// fs.writeFile('json/response.json', format_response_json)
+			client.get('statuses/user_timeline', params, function(error, tweets, response){
 
-			return tweets_preformat(tweets);
+				// // ローカルテスト用
+				// const format_tweets_json = JSON.stringify(tweets);
+				// const format_response_json = JSON.stringify(response)
+				// fs.writeFile('json/tweets.json', format_tweets_json)
+				// fs.writeFile('json/response.json', format_response_json)
+
+				resolve(tweets_preformat(tweets));
+
+			});
 
 		});
 
@@ -46,42 +50,47 @@ app.get('/', function(req, res) {
 
 	function tweets_preformat(tweets){
 
-		let all_tweet_text = '';
+		return new Promise(function(resolve){
 
-		tweets.forEach(function(tweet){
-			all_tweet_text = all_tweet_text + tweet['text'];
-		});
+			let all_tweet_text = '';
 
-		let all_tweet_word = '';
-		let words_freq = [['count', 'word']];
-		let exist_words = [];
-		let mecab = new MeCab();
-		let escape_words = ['RT', 'http', 'https', '://', ':', ';', '@', '@_', '：@_', '#', '/', '.', 'D'];
-
-		all_tweet_text = all_tweet_text.replace(/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- ./?%&=]*)?/g, '')
-
-		mecab.parse(all_tweet_text, function(err, words_info) {
-
-			words_info.forEach(function(word_info){
-				if(word_info[1] === '名詞' && isNaN(word_info[0]) && escape_words.indexOf(word_info[0]) === -1){
-					let word_order = exist_words.indexOf(word_info[0])
-					if(word_order === -1){
-						words_freq.push([1, word_info[0]])
-						exist_words.push(word_info[0])
-					}
-					else {
-						words_freq[word_order+1][0] = words_freq[word_order+1][0] + 1;
-					}
-				};
+			tweets.forEach(function(tweet){
+				all_tweet_text = all_tweet_text + tweet['text'];
 			});
-			return words_freq;
+
+			let all_tweet_word = '';
+			let words_freq = [['count', 'word']];
+			let exist_words = [];
+			let mecab = new MeCab();
+			let escape_words = ['RT', 'http', 'https', '://', ':', ';', '@', '@_', '：@_', '#', '/', '.', 'D'];
+
+			all_tweet_text = all_tweet_text.replace(/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- ./?%&=]*)?/g, '')
+
+			mecab.parse(all_tweet_text, function(err, words_info) {
+
+				words_info.forEach(function(word_info){
+					if(word_info[1] === '名詞' && isNaN(word_info[0]) && escape_words.indexOf(word_info[0]) === -1){
+						let word_order = exist_words.indexOf(word_info[0])
+						if(word_order === -1){
+							words_freq.push([1, word_info[0]])
+							exist_words.push(word_info[0])
+						}
+						else {
+							words_freq[word_order+1][0] = words_freq[word_order+1][0] + 1;
+						}
+					};
+				});
+				resolve(words_freq);
+
+			});
 
 		});
 
 	};
 
-	// var tweets = get_tweets();
-	res.json({ 'body': 'test' });
+	get_tweets().then(function(tweets){
+		res.json({ 'body': tweets });
+	});
 
 });
 
