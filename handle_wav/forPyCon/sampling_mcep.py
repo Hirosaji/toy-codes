2a538d8641ed2993ac3167b47ea5e661d09ac61e
Hiroya Kato <hiroya.kato@nex.nikkei.com>
2019-09-12 01:04:00 +0900

    refactor sampling_mcep.py

--- handle_wav/forPyCon/sampling_mcep.py
+++ handle_wav/forPyCon/sampling_mcep.py
@@ -1,5 +1,5 @@
-# #!/usr/bin/python
-# # coding: utf-8
+#!/usr/bin/python
+# coding: utf-8
 import numpy as np
 import pysptk as sptk
 import matplotlib.pyplot as plot
@@ -10,54 +10,52 @@ from pysas import excite
 from pysptk.synthesis import MLSADF, Synthesizer
 
 def AddWhiteNoise(x, rate=0.02):
-    #ホワイトノイズを乗せる処理
-    x=x.astype(np.float64)
-    return x+rate*np.random.randn(len(x))
+    x = x.astype(np.float64)
+    return x + rate * np.random.randn(len(x))
 
-#読み込むファイル
-wavefile='wav/sample02.wav'
+# read wav file
+wavefile = “wav/sample02.wav”
+fs, x = wavfile.read(wavefile) # fs: sampling rate, x: wave data
 
-#wavファイル読み込み
-fs, x=wavfile.read(wavefile) #fs=サンプリングレート、x=波形データ
+# add white noise on target wav data
+data = AddWhiteNoise(x)
 
-#波形データにホワイトノイズを乗せる
-data=AddWhiteNoise(x)
+# multiplicate window func
+frame_length = 1024
+hop_length = 80
+frames = librosa.util.frame(data, frame_length=frame_length, hop_length=hop_length).astype(np.float64).T
+frames *= sptk.blackman(frame_length)
 
-#ウィンドウ関数をかける
-frame_length=1024
-hop_length=80
-frames=librosa.util.frame(data, frame_length=frame_length, hop_length=hop_length).astype(np.float64).T
-frames*=sptk.blackman(frame_length)
+# estimate F0
+f0 = sptk.swipe(data.astype(np.float64), fs=fs, hopsize=hop_length, min=25, max=2000)
+generator = excite.ExcitePulse(fs, hop_length, False)
+source_excitation = generator.gen(f0)
 
-#F0解析
-f0=sptk.swipe(data.astype(np.float64), fs=fs, hopsize=hop_length, min=25, max=2000)
-generator=excite.ExcitePulse(fs, hop_length, False)
-source_excitation=generator.gen(f0)
+# params
+order = 25
+alpha = 0.41
 
-order=25
-alpha=0.41
+# estimate mel cepstrum (MCEP)
+mc = sptk.mcep(frames, order, alpha)
+synthe = Synthesizer(MLSADF(order=order, alpha=alpha), hop_length)
 
-#mcep解析
-mc=sptk.mcep(frames, order, alpha)
-synthe=Synthesizer(MLSADF(order=order, alpha=alpha), hop_length)
+# synthesize audio data from acoustic features
+b = sptk.mc2b(mc, alpha)
+xsynthe = synthe.synthesis(source_excitation, b).astype(np.int16)
 
-#解析結果から波形を生成する
-b=sptk.mc2b(mc, alpha)
-xsynthe=synthe.synthesis(source_excitation, b).astype(np.int16)
-
-#生成した波形を出力
+# write audio data as wav file
 wavfile.write('xsynthe.wav', fs, xsynthe)
 print('xsynthe')
 
-#オリジナル波形
+# plot original wav data
 plot.subplot(3,1,1)
 plot.plot(x)
 
-#生成波形
+# plot synthesized audio data
 plot.subplot(3,1,2)
 plot.plot(xsynthe)
 
-#2つを重ねたもの
+# plot origin & synthesized
 plot.subplot(3,1,3)
 plot.plot(x)
 plot.plot(xsynthe)
