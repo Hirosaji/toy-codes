32096b11606050294f9c98bc658fb6e8c9e6e333
Hiroya Kato <hiroya.kato@nex.nikkei.com>
2019-11-13 18:45:16 +0900

    add rewrite SVG func [WIP]

--- chrome_extension_sample/content.js
+++ chrome_extension_sample/content.js
@@ -1,5 +1,56 @@
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    console.log(message);
-    console.log(JSON.parse(message).contents.target);
-    return true;
-});
+(function () {
+    chrome.runtime.onMessage.addListener(function (req, _, _) {
+        const data = JSON.parse(req);
+
+        const target = data.contents.target;
+        const width = data.contents.width;
+        const height = data.contents.height;
+
+        const s = d3.select(target);
+
+        const sHTML = [s.html()].slice()[0];
+        const tClass = s.attr("class");
+        const tId = s.attr("id");
+
+        s.html("");
+
+        s.attr("class", "__blank__")
+            .attr("id", "__blank__");
+
+        s.append("div")
+            .attr("class", tClass)
+            .attr("id", tId)
+            .html(sHTML);
+
+        s
+            .attr("width", width)
+            .attr("height", height)
+            .style("max-width", width + "px")
+            .style("width", width + "px")
+            .style("max-height", height + "px")
+            .style("height", height + "px");
+
+        s.select(target)
+            .attr("width", width)
+            .attr("height", height)
+            .style("max-width", width + "px")
+            .style("width", width + "px")
+            .style("max-height", height + "px")
+            .style("height", height + "px");
+
+        s.select(target)
+            .select("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .style("max-width", width + "px")
+            .style("width", width + "px")
+            .style("max-height", height + "px")
+            .style("height", height + "px");
+
+        // console.log(target, width, height);
+
+        window.dispatchEvent(new Event("resize"));
+
+        return true;
+    });
+})();
